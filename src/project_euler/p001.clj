02860643c63f1c04d9ec88a035c94b5f268986d5
Debetimi Okah <debetimi@gmail.com>
2015-07-22 09:37:03 -0700

    updated

--- src/project_euler/p001.clj
+++ src/project_euler/p001.clj
@@ -1,23 +1,21 @@
 (ns project-euler.p001)
+;;;  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+;;;  The sum of these multiples is 23.
+;;;  Find the sum of all the multiples of 3 or 5 below 1000."
 
 (defn evenly-spaced-series-sum
   "Returns the sum of evenly spaced series that starts at 0"
-  [spacing n]
-  (*' (/ (+' spacing (*' spacing n)) 2) n))
+  [delta n]
+  (*' (/ (+' delta (*' delta n)) 2) n))
 
 
 (defn sum-multiples-of-3-and-5
-  "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-   The sum of these multiples is 23.
-   Find the sum of all the multiples of 3 or 5 below 1000."
+  "Returns sums of multiples of three and five that are less n"
   [n]
-  (let [n (dec n)
-        mult3 (quot n 3) 
-        mult5 (quot n 5) 
-        mult15 (quot n 15)]
-    (long (+' (evenly-spaced-series-sum 3 mult3) 
-              (evenly-spaced-series-sum 5 mult5) 
-              (- (evenly-spaced-series-sum 15 mult15))))))
+  (let [n (dec n)]
+    (long (+' (evenly-spaced-series-sum 3 (quot n 3)) 
+              (evenly-spaced-series-sum 5 (quot n 5)) 
+              (- (evenly-spaced-series-sum 15 (quot n 15)))))))
 
 (defn -main
   "Main function for Hackerrank Code"
