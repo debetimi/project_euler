f48e69afb6022f95a69a5be85fff0e31e5125055
Debetimi Okah <debetimi@gmail.com>
2015-07-24 01:39:04 -0700

    added question text

--- src/project_euler/p014.clj
+++ src/project_euler/p014.clj
@@ -1,8 +1,16 @@
 (ns project-euler.p014
   (:require [clojure.math.numeric-tower :as math])) 
 
+;;; The following iterative sequence is defined for the set of positive integers:
+;;; n → n/2  (n is even)
+;;; n → 3n + 1  (n is odd)
+;;; Using the rule above and starting with 13, we generate the following sequence:
+;;; 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+;;; It can be seen that this sequence  (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet  (Collatz Problem), it is thought that all starting numbers finish at 1.
+;;; Which starting number, under one million, produces the longest chain?
+;; NOTE: Once the chain starts the terms are allowed to go above one million.
 
-(def cache (atom {1 1}))
+(def cache (atom {}))
 
 (defn len-collatz
   "Calculates the length of a collatz series starting at n"
@@ -10,28 +18,20 @@
   (if (contains? @cache n) 
     (get @cache n)
     (let [v (cond 
-              (= 1 n) n
+              (= 1 n) 1
               (even? n) (+ 1 (len-collatz (/ n 2)))
-              :else (+ 1 (len-collatz (+ 1 (* 3 n)))))]
+              :else (+ 2 (len-collatz (/ (+ 1 (* 3 n)) 2))))]
       (swap! cache assoc n v)
       v))) 
 
 (defn longest-collatz
-  "Returns number that produces longest collatz series up to max-seed"
+  "Returns number that produces longest collatz series in the range [1 max-seed]"
   [max-seed]
   (let [max-len (atom -1)
         max-n (atom -1)]
-    (dotimes [n max-seed]
-      (let [n (inc n)
-            len (len-collatz n)]
+    (doseq [n (range 1 (inc max-seed))]
+      (let [len (len-collatz n)]
         (when (> len @max-len) 
           (reset! max-len len)
           (reset! max-n n))))
     @max-n)) 
-
-
-(defn solve[]
-  ;; Warm up solver to avoid stack over flow
-  (doseq [n (range 1 5)]
-    (longest-collatz (math/expt 10 n)))
-  (longest-collatz 1e6))
