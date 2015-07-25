e0ec182e4aeb7fcf8e8a6835f044a84ee8ea7b94
Debetimi Okah <debetimi@gmail.com>
2015-07-25 00:52:10 -0700

    p15

---
+++ src/project_euler/p015.clj
@@ -0,0 +1,17 @@
+(ns project-euler.p015)
+
+;;; https://projecteuler.net/problem=15 
+;;; Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+;;; How many such routes are there through a 20×20 grid?
+
+(defn lattice-paths
+  "Finds the number of lattice paths"
+  [dim]
+  (let [paths (atom {})]
+    (dotimes [i (inc (first dim))] 
+      (dotimes [j (inc (second dim))] 
+        (let [weight (cond
+                       (= [0 0] [i j]) 1
+                       :else (+ (get @paths [(dec i) j] 0) (get @paths [i (dec j)] 0)))]
+          (swap! paths assoc [i j] weight))))
+    (get @paths dim)))
