87ce88ada73cb919ea9abbf7d0a74c2ca24e3b39
Debetimi Okah <debetimi@gmail.com>
2015-07-30 00:25:31 -0700

    renamed

--- src/project_euler/p019.clj
+++ src/project_euler/p019.clj
@@ -1,5 +1,17 @@
 (ns project-euler.p019)
 
+;;; You are given the following information, but you may prefer to do some research for yourself.
+;;; 
+;;; 1 Jan 1900 was a Monday.
+;;; Thirty days has September,
+;;; April, June and November.
+;;; All the rest have thirty-one,
+;;; Saving February alone,
+;;; Which has twenty-eight, rain or shine.
+;;; And on leap years, twenty-nine.
+;;; A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+;;; How many Sundays fell on the first of the month during the twentieth century  (1 Jan 1901 to 31 Dec 2000)?
+
 (def months [:Jan :Feb :Mar :Apr :May :June :July :Aug :Sept :Oct :Nov :Dec])
 (def days-of-week [:Sun :Mon :Tues :Wed :Thur :Fri :Sat])
 
@@ -16,7 +28,7 @@
     :Feb (if (leap-year? year) 29 28)
     31))
 
-(defn days-since
+(defn days-from
   "lazy sequence of days starting at start"
   [start]
   (letfn [(get-date [date]
@@ -36,5 +48,5 @@
                             (and (= :Sun (:day-of-week date)) (= 1 (:day date))))
         start {:day 1, :month :Jan :year 1900 :day-of-week :Mon}
         twentieth-century? #(> 2001 (:year %))
-        jan-1-1901 (first (drop-while #(> 1901 (:year %)) (days-since start)))]
-    (count (filter sunday-and-first? (take-while twentieth-century? (days-since jan-1-1901))))))
+        jan-1-1901 (first (drop-while #(> 1901 (:year %)) (days-from start)))]
+    (count (filter sunday-and-first? (take-while twentieth-century? (days-from jan-1-1901))))))
