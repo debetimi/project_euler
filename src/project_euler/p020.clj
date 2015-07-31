077c3ac807c40490167e3fc88986d31359175dc8
Debetimi Okah <debetimi@gmail.com>
2015-07-31 02:43:12 -0400

    problem 18

--- src/project_euler/p020.clj
+++ src/project_euler/p020.clj
@@ -1,4 +1,4 @@
 (ns project-euler.p020)
 
-(def solve [] (-> (reduce *' (range 1 101)) str (clojure.string/split #"") rest ((partial map read-string)) ((partial reduce +')))) 
+(defn solve [] (-> (reduce *' (range 1 101)) str (clojure.string/split #"") rest ((partial map read-string)) ((partial reduce +')))) 
 
