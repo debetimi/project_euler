d0c0dc27c5f0cd9d0fc798120c68eb66040a7a7e
Debetimi Okah <debetimi@gmail.com>
2015-08-05 12:07:20 -0400

    p25

--- src/project_euler/p025.clj
+++ src/project_euler/p025.clj
@@ -1,4 +1,6 @@
 (ns project-euler.p025
   (:require [project-euler.p002 :as p002]))
 
-(defn solve [] (first (drop-while #(> 1000 (count (str %) )) p002/fibonacci)))
+;;; What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+(defn solve [] (count (take-while #(> 1000 (count (str %) )) p002/fibonacci)))
