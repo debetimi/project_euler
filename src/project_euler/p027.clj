bd28bbf430620bdd0119b919d79109709a175e47
Debetimi Okah <debetimi@gmail.com>
2015-08-06 13:23:07 -0400

    foo

--- src/project_euler/p027.clj
+++ src/project_euler/p027.clj
@@ -2,17 +2,14 @@
   (:require [project-euler.p003 :as p003]
             [project-euler.p007 :as p007]))
 
-(defn prime? [x] (= 1 (count (p003/prime-factors x))))
-
-(defn quadratic [a b] (fn [n] (+' (*' n n) (*' a n) b)))
-
 (defn solve []
   (let [most-primes (atom -1)
         best-pair (atom nil)
+        prime? (fn [x] (= 1 (count (p003/prime-factors x))))
         primes (take-while (partial > 1001) (p007/lazy-primes3))]
     (doseq [a (range -999 1001 2)]
       (doseq [b primes]
-        (let [f (quadratic a b)
+        (let [f (fn [n] (+' (*' n n) (*' a n) b))
               num-primes (count (take-while prime? (map f (range))))]
           (when (> num-primes @most-primes)
             (reset! most-primes num-primes)
