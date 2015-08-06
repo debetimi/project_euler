efd16308750a7c278d0988ef8eaf5a02e9858fc2
Debetimi Okah <debetimi@gmail.com>
2015-08-06 12:54:52 -0400

    push

--- src/project_euler/p027.clj
+++ src/project_euler/p027.clj
@@ -1,5 +1,6 @@
 (ns project-euler.p027
   (:require [project-euler.p003 :as p003]
+            [project-euler.p007 :as p007]
             [clojure.math.combinatorics :as combinatorics]))
 
 (defn prime? [x] (= 1 (count (p003/prime-factors x))))
@@ -8,9 +9,10 @@
 
 (defn solve []
   (let [most-primes (atom -1)
-        best-pair (atom nil)]
-    (doseq [a (range -1000 1001)]
-      (doseq [b (range -1000 1001)]
+        best-pair (atom nil)
+        primes (take-while (partial > 1001) (p007/lazy-primes3))]
+    (doseq [a (range -999 1001 2)]
+      (doseq [b primes]
         (let [f (quadratic a b)
               num-primes (count (take-while prime? (map f (range))))]
           (when (> num-primes @most-primes)
