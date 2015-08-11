1c7b4623b4e344db1e29921d5a4c5fc6797a1635
Debetimi Okah <debetimi@gmail.com>
2015-08-10 22:59:08 -0400

    p31

---
+++ src/project_euler/p031.clj
@@ -0,0 +1,21 @@
+(ns project-euler.p031)
+
+;;; In England the currency is made up of pound, £, and pence, p, and there are eight coins in general circulation:
+;;; 1p, 2p, 5p, 10p, 20p, 50p, £1  (100p) and £2  (200p).
+;;; It is possible to make £2 in the following way:
+;;; 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+;;; How many different ways can £2 be made using any number of coins?
+
+(def currency [1 2 5 10 20 50 100 200])
+
+(defn num-ways-to-make-change
+  [amt]
+  (let [ways (atom (vec (repeat amt 0)))]
+    (doseq [coin currency]
+      (doseq [n (range (dec coin) amt)]
+        (let [curr (get @ways n)
+              increment (if (= n (dec coin)) 1 (get @ways (- n coin)))]
+          (swap! ways assoc n (+ curr increment)))))
+    (get @ways (dec amt))))
+
+(defn solve [] (num-ways-to-make-change 200))
