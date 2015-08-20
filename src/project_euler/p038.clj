525218b025dbbe4a49e1d85cc1458a5d9615e6e5
Debetimi Okah <debetimi@gmail.com>
2015-08-20 08:57:00 -0400

    refactored since only 4

--- src/project_euler/p038.clj
+++ src/project_euler/p038.clj
@@ -1,5 +1,5 @@
 (ns project-euler.p038
-  (:require [project-euler.utils :as utils]
+  (:require [project-euler.utils :refer [digits->num]]
             [clojure.math.combinatorics :as combo]))
 
 ;;; Take the number 192 and multiply it by each of 1, 2, and 3:
@@ -10,16 +10,12 @@
 ;;; The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
 ;;; What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?
 
-(defn pandigital? [digits]
-  (let [multiplier (utils/digits->num (take 4 digits))] ;; we know it has to be a 4 digit number since 2/3 digits would never produce exactly 9 digits 
-    (loop [multiplicand 1 d digits]
-      (if (empty? d) 
-        (utils/digits->num digits)
-        (let [prod (utils/num->digits (* multiplier multiplicand))
-              len (count prod)]
-          (if (= prod (take len  d))
-            (recur (inc multiplicand) (drop len d))
-            false))))))
+;; We know it has to be a 4 digit number since 2 
+;; digits would produce number so 2 3 3 4 respectively
+;; and 3 digits would produce numbers of 3 4 4 respectively
+;; both of which is exactly 9 digits 
+(defn pandigital? [digits] 
+  (= (* 2 (digits->num (take 4 digits))) (digits->num (drop 4 digits))))
 
 (defn solve []
-  (first (filter pandigital? (combo/permutations (reverse (range 1 10))))))
+  (digits->num (first (filter pandigital? (combo/permutations (reverse (range 1 10)))))))
