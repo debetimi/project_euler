86f0765f6eb9ef81543897dd51e3d753e9b3c1aa
Debetimi Okah <debetimi@gmail.com>
2015-08-22 14:09:17 -0400

    foo

--- src/project_euler/p040.clj
+++ src/project_euler/p040.clj
@@ -8,10 +8,6 @@
 ;If dn represents the nth digit of the fractional part, find the value of the following expression.
 ;d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
 
-;; TODO - write function called nths that takes a collection of indexes to retur
-;; and iterates once
-
-
 (defn solve []
   (let [indexes (map (comp dec (partial math/expt 10)) (range))]
     (reduce * (nths (flatten (map (comp num->digits inc) (range))) (take 7 indexes)))))
