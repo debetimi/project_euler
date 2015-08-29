bd6573dd424b626f2db64f5521cada5ff5f5679c
Debetimi Okah <debetimi@gmail.com>
2015-08-29 11:44:26 -0400

    foo

--- src/project_euler/p171.clj
+++ src/project_euler/p171.clj
@@ -44,26 +44,19 @@
 
 (defn collector [max-digits] 
   (let [h (fn helper [v d sq]
-            (println "loop" v d sq)
             (let [remaining (- v sq)]
               (cond (contains? @cache [v sq]) (get @cache [v sq]) 
                     (neg? remaining) [] 
                     (neg? d) []
-                    (zero? remaining)(do (println "doing summand")[(->Summands [(sqrt v)])]) 
+                    (zero? remaining)(do [(->Summands [(sqrt v)])]) 
                     :else (let [cached (filter #(contains? @cache [remaining %]) (take-while (partial >= remaining) (map square (range 0 10))))
                                 uncached (take-while (partial >= remaining) (remove (set cached) (map square (range 0 10))))
-                                _ (println "uncached" uncached)
                                 cached-base (mapcat #(get @cache [remaining %]) cached) 
-                                _ (println "cached-base" cached-base)
                                 base (mapcat (partial helper remaining (dec d)) (reverse uncached))
-                                _ (println "base" base)
                                 base2 (concat cached-base base)
-                                _ (println "base2" base2)
-                                _ (println "filtered" (filter #(>= max-digits (len %)) (map #(add % sq) (map reprotect base2))))
                                 baz (filter #(>= max-digits (len %)) (map #(add % sq) (map reprotect base2))) 
-                                _ (println "baz" baz)
                                 ]
-                            (when (not= 0 sq) (swap! cache assoc [v sq] baz))
+                            (when (not= -10 sq) (swap! cache assoc [v sq] baz))
                             baz
                             #_(get (swap! cache assoc [v sq] (filter #(>= max-digits (len %)) (map #(add % sq) (map reprotect base2))))
                                  [v sq])))))
@@ -72,13 +65,14 @@
     h))
 
 (comment (do (def squares (possible-squares 1e20))
+             (def squares (possible-squares 1e10))
              (def digits (map square (range 10)))
              (def cache (atom {}))
-             (def helpfn (collector (max-digits 1e20)))
-             (def padfn (partial zero-padded-variants (max-digits 1e20))))
+             (def helpfn (collector 9))
+             (def padfn (partial zero-padded-variants 9)))
          (map #(helpfn % 25) (range 1 55))
          (time (count (reduce +' (map unwrap (flatten (map padfn (flatten (map #(map (partial helpfn %) digits) squares))))))))
-         (time (count (reduce +' (map (comp unwrap padfn) (flatten (map #(map (partial helpfn % 18) digits) (range 4 5) #_(take 7 squares)))))))
+         (time (count (reduce +' (map (comp unwrap padfn) (flatten (map #(map (partial helpfn % 8) digits) (take 7 squares)))))))
          (time (count (reduce +' (sort (map unwrap (flatten (flatten (def foo (map #(map (partial helpfn % 18) digits) (range 4 5) #_(take 7 squares))))))))))
          (time (count (reduce +' (sort (count (map unwrap (flatten (map #(map (partial helpfn %) digits) [16] #_(take 3 squares)))))))))
          (time (count (map zero-padded-variants (take 250 (map unwrap (flatten (map #(map (partial helpfn %) digits) squares)))))))
