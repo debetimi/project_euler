1dd56447e78a32670d771298e2eb5b6bbddd1417
Debetimi Okah <debetimi@gmail.com>
2015-08-29 20:48:53 -0400

    formatting

--- src/project_euler/p046.clj
+++ src/project_euler/p046.clj
@@ -1,10 +1,9 @@
 (ns project-euler.p046
   (:require [project-euler.utils :refer [lazy-primes prime?]]
-            [clojure.math.numeric-tower :refer [sqrt]]
-            ))
+            [clojure.math.numeric-tower :refer [sqrt]]))
 
 (defn goldbach? [n]
   (some? (first (filter #(integer? (sqrt (/ (- n %) 2))) (take-while (partial >= n) lazy-primes)))))
 
 (defn solve []
-  (first (drop-while goldbach? (filter (every-pred odd? (complement (fn [x] (.isProbablePrime (biginteger x) 5)))) (map #(+ 33 (* 2 %)) (range))))))
+  (first (drop-while goldbach? (filter (every-pred odd? (complement prime?)) (map #(+ 33 (* 2 %)) (range))))))
